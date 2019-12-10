fa2f9eeb13e06c798141220447d343ca3a1cdaef
Zero <44189765+Zero851116626@users.noreply.github.com>
2019-12-11 00:18:08 +0800

    配置css编译器

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -47,6 +47,7 @@ const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
+const stylusRegex = /\.styl$/;
 
 // This is the production and development configuration.
 // It is focused on developer experience, fast rebuilds, and a minimal bundle.
@@ -488,6 +489,18 @@ module.exports = function(webpackEnv) {
                 'sass-loader'
               ),
             },
+            {
+              test: stylusRegex, 
+              loaders: ['style-loader', 'css-loader', 'stylus-loader']
+            },
+            {
+              test: /\.styl$/,
+              use: [
+                require.resolve('style-loader'),
+                require.resolve('css-loader'),
+                require.resolve('stylus-loader')
+              ]
+            },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
             // In production, they would get copied to the `build` folder.
@@ -499,7 +512,7 @@ module.exports = function(webpackEnv) {
               // its runtime that would otherwise be processed through "file" loader.
               // Also exclude `html` and `json` extensions so they get processed
               // by webpacks internal loaders.
-              exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+              exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/ , /\.styl$/],
               options: {
                 name: 'static/media/[name].[hash:8].[ext]',
               },
